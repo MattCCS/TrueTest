c43ff49d4031d52ff692e8d3b2bb520e3d820b96
Matthew Cotton <matt@thecottons.com>
2016-03-23 18:26:20 -0400

    'Initial commit (auto)'

---
+++ setup.py
@@ -0,0 +1,20 @@
+"""
+Auto-generated setup.py file.
+"""
+
+from distutils.core import setup
+
+setup(
+    name='truetest',
+    version='0.0.1',
+    author='Matthew Cotton',
+    author_email='matt@thecottons.com',
+    packages=['truetest'],
+    # py_modules=[],
+    # scripts=[],
+    url='http://pypi.python.org/pypi/truetest/',
+    license='LICENSE.txt',
+    description='"That\'s a complicated \'description\'," she said.',
+    long_description=open('README').read(),
+    install_requires=[],
+)
